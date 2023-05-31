d814b70d9117b4414dee74e971387c5e7a2178f0
Niclas Älmeby <noreply>
2023-05-31 07:03:18 +0200

    fixed bash helper file

--- create_github_workflow_sp.sh
+++ create_github_workflow_sp.sh
@@ -3,4 +3,5 @@
 read -p "Subscription id: " subscription_id
 
 sp_name="GithubAction"
-az ad sp create-for-rbac --name $sp_name --role contributor --scopes "/subscriptions/$subscription_id" --sdk-auth | jq '. | {clientId: .clientId, clientSecret: .clientSecret, subscriptionId: .subscriptionId,tenantId: .tenantId}
+
+az ad sp create-for-rbac --name $sp_name --role contributor --scopes "/subscriptions/$subscription_id" --sdk-auth | jq '. | {clientId: .clientId, clientSecret: .clientSecret, subscriptionId: .subscriptionId,tenantId: .tenantId}'
